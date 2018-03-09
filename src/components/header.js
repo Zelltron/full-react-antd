68b1c7f67ddb97dbbec2c9f83eadebec493d0e9b
Ryan Zell <rzell@EUCLID2003.local>
2018-03-09 11:45:56 -0500

    add antd

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Menu, Icon } from 'antd';
 
-import logo from '../images/logo.svg';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+
 
 class Header extends Component {
-
+  state = {
+  current: 'mail',
+  }
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
   // these links can be conditional
   renderLinks() {
     return [
-      <li className="nav-item" key={1}>
+      <Menu.Item className="nav-item" key={1}>
         <Link className="nav-link" to="/Page2">Page-2</Link>
-      </li>
+      </Menu.Item>
     ];
   }
 
   render() {
     return (
-      <div>
-        <img className="logo" src={logo} />
-        <nav className="navbar navbar-light">
-          <Link to="/" className="navbar-brand">Redux Home</Link>
-          <ul className="nav navbar-nav">
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+         <Menu.Item className="nav-item" key={2}>
+           <Link to="/" className="navbar-brand">Redux Home</Link>
+          </Menu.Item>
           {this.renderLinks()}
-          </ul>
-        </nav>
-      </div>
+      </Menu>
     );
   }
 }
